f91c0565b1dc2004045c473b1ea776a0fb5e50d9
Andrey <42993931+BobrovAndrey@users.noreply.github.com>
2019-02-09 14:33:14 +0200

    undo all changes

--- server.ts
+++ server.ts
@@ -1,17 +1,19 @@
-import * as express from "express";
-const app = express();
+import express = require('express')
+const app = express()
 
 
-app.get("/", (req, res, next) => {
-    res.send("TourBooking API");
-});
+app.get('/', (req, res, next)=> {
+    res.send('Tour Booking API')
+})
 
-app.get("/tours", (req, res, next) => {
-    res.send("Get a list of tours");
-});
+app.get('/tours', (req, res, next)=> {
+    res.send('Get list of tours')
+})
 
-app.post("/tours", (req, res, next) => {
-    res.send("Post a new tour");
-});
+app.post('/tours', (req, res, next)=> {
+    res.send('Add a new tour...')
+})
 
-app.listen(process.env.PORT || 8091, () => console.log("Server Started..."));
+console.log('test change')
+
+app.listen(process.env.PORT || 8091, () => console.log('Server runing...'))
