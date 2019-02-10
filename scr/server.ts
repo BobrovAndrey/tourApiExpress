af7b9e8ddae7119c8a3e7e08b4e9220a82130d1e
Andrey <42993931+BobrovAndrey@users.noreply.github.com>
2019-02-10 14:24:31 +0200

    fix example

--- scr/server.ts
+++ scr/server.ts
@@ -1,19 +1,31 @@
-import express = require('express')
-const app = express()
+// import express = require('express')
+// const app = express()
 
 
-app.get('/', (req, res, next)=> {
-    res.send('Tour Booking API')
-})
+// app.get('/', (req, res, next)=> {
+//     res.send('Tour Booking API')
+// })
 
-app.get('/tours', (req, res, next)=> {
-    res.send('Get list of tours')
-})
+// app.get('/tours', (req, res, next)=> {
+//     res.send('Get list of tours')
+// })
 
-app.post('/tours', (req, res, next)=> {
-    res.send('Add a new tour...')
-})
+// app.post('/tours', (req, res, next)=> {
+//     res.send('Add a new tour...')
+// })
 
-console.log('test change')
+// console.log('test change')
 
-app.listen(process.env.PORT || 8091, () => console.log('Server runing...'))
+// app.listen(process.env.PORT || 8091, () => console.log('Server runing...'))
+import * as express from "express";
+import log from "./log";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json(JSON.stringify({ok: 1})).end();
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    log.info("app running");
+});
