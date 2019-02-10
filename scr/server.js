a0212ba1c9a41a3ec66476e10a3c9a87347c2488
Andrey <42993931+BobrovAndrey@users.noreply.github.com>
2019-02-10 14:37:28 +0200

    trying another example

--- scr/server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res, next) {
-    res.send('Tour Booking API');
-});
-app.listen(process.env.PORT || 8091, function () { return console.log('Server runing...'); });
