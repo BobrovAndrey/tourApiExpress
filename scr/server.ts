9836c9a6252370cfff0e7fb54772572b2656b3fd
Andrey <42993931+BobrovAndrey@users.noreply.github.com>
2019-02-10 16:19:51 +0200

    working example

--- scr/server.ts
+++ scr/server.ts
@@ -1,23 +1,23 @@
-// import express from 'express';
-// const app = express()
-
-
-// app.get('/', (req, res, next)=> {
-//      res.send('Tour Booking API')
-//  })
-
-//  app.get('/tours', (req, res, next)=> {
-//      res.send('Get list of tours')
-// })
-
-//  app.post('/tours', (req, res, next)=> {
-//      res.send('Add a new tour...')
-//  })
-
-//  app.listen(process.env.PORT || 8091, () => console.log('Server runing...'))
 import express from 'express';
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.get('/', (req, res, next)=> {
+     res.send('Tour Booking API')
+ })
+
+ app.get('/tours', (req, res, next)=> {
+     res.send('Get list of tours')
+})
+
+ app.post('/tours', (req, res, next)=> {
+     res.send('Add a new tour...')
+ })
+
+ app.listen(process.env.PORT || 8091, () => console.log('Server runing...'))
+// import express from 'express';
+// const app = express();
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
