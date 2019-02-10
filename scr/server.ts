024c2cacb73cc7f8d8fd7a0a6a107530f5ecbbce
Andrey <42993931+BobrovAndrey@users.noreply.github.com>
2019-02-10 16:52:12 +0200

    listen answer changed

--- scr/server.ts
+++ scr/server.ts
@@ -14,10 +14,4 @@ app.get('/', (req, res, next)=> {
      res.send('Add a new tour...')
  })
 
- app.listen(process.env.PORT || 8091, () => console.log('Server runing...'))
-// import express from 'express';
-// const app = express();
-
-// app.get('/', (req, res) => res.send('Hello World!'));
-
-// app.listen(3000, () => console.log('Example app listening on port 3000!'));
+ app.listen(process.env.PORT || 8091, () => console.log('Server now runing...'))
